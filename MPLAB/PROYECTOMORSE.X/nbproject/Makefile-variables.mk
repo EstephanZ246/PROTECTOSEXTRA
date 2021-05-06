2918f8e60a6d146c8cd968c3b78c75d3fae5b3a1
Estephan2001 <franciscoestephan@gmail.com>
2021-05-06 10:31:07 -0600

    FINAL

---
+++ MPLAB/PROYECTOMORSE.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=PROYECTOMORSE.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/PROYECTOMORSE.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=proyectomorse.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/proyectomorse.x.tar
